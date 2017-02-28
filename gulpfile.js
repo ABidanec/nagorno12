e8d133ef65a5ce62f4d123a170c53d5283b058d8
Bidanec Alexandr <a.bidanec@ukr.net>
2017-02-28 15:41:59 +0200

    Верстка блока b-responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	browserSync = require('browser-sync');
+	browserSync = require("browser-sync");
 
-// sass
-gulp.task('sass', function(){
-	gulp.src('app/sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('app/css'));
+// компиляция sass
+gulp.task('sass', function(){ 
+	gulp.src("app/sass/**/*.scss")
+		.pipe(sass().on('error', sass.logError)) // .on('error', sass.logError) - чтобы не было фатальной ошибки при синтаксической ошибки в sass файле, а только уведомление
+		.pipe(gulp.dest("app/css"));
 });
 
-// browserSync
 gulp.task('browser-sync', function(){
 	browserSync({
 		server : {
-			baseDir : 'app'
+			baseDir : "app"
 		},
-		notify : false
+		notify: false
 	})
 });
 
-// watch
-gulp.task('watch', ['browser-sync'], function(){
+// таск слежения
+// gulp.task("sass:watch", function(){
+// 	gulp.watch('app/sass/**/*.scss', ['sass']);
+// });
+
+gulp.task("watch", ['browser-sync'], function(){
 	gulp.watch('app/sass/**/*.scss', ['sass']);
 	gulp.watch('app/js/**/*.js', browserSync.reload);
 	gulp.watch('app/css/**/*.css', browserSync.reload);
